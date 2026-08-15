aa4470d8aaf745e559ddbc772fe6b404e9eab9a8
Adhivp <adhivp910@gmail.com>
2026-08-15 18:13:54 +0530

    migrated strapi's sql lite and inubilt storage to neon and cloudflare r2

--- strapi-backend/config/middlewares.js
+++ strapi-backend/config/middlewares.js
@@ -1,7 +1,20 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'media.adhivp.com', '*.r2.cloudflarestorage.com', '*.r2.dev'],
+          'media-src': ["'self'", 'data:', 'blob:', 'media.adhivp.com', '*.r2.cloudflarestorage.com', '*.r2.dev'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
